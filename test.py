905dfefb92881020278c3f38314c3135d556191f
gopancakes <samuelfdove@gmail.com>
2022-12-18 13:41:03 -0600

    test changes

--- test.py
+++ test.py
@@ -4,15 +4,11 @@ from Turn import *
 from Balls import *
 import random
 
-# t = Turn()
-# while True:
-#   t.reset()
-#   print(t.ballstate)
-#   t.taketurn(random.randint(1000,2500)/1000,random.randint(1000,2500)/1000)
-#   #t.taketurn(3,3)
-#   pygamemain()
+t = Turn()
+while True:
+  t.reset()
+  print(t.ballstate)
+  t.taketurn(random.randint(1000,2500)/1000,random.randint(1000,2500)/1000)
+  #t.taketurn(3,3)
+  pygamemain()
 
-print(np.asarray(findballpos()))
-print(
-  Balls(np.asarray(findballpos()))
-)
